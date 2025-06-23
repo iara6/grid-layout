02df059bbe24d844b5cf7d383c75de95af0ed017
Irene <grim6ace@gmail.com>
2025-06-23 23:56:52 +0300

    Ver 13: Dark mode

--- grid-layout.js
+++ grid-layout.js
@@ -2,3 +2,12 @@
 
 const date = document.getElementById("copyright-date");
 date.innerHTML = new Date().getFullYear();
+
+/* Dark mode button */
+
+const switchBtn = document.querySelector('.switch-button');
+
+switchBtn.addEventListener('click', () => {
+  const darkModeOn = switchBtn.classList.toggle('slide');
+  document.documentElement.classList.toggle('dark-mode', darkModeOn); /* toggle('class', condition);documentElement returns <html> */
+});
