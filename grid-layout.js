b7548ec55f89eaad6b6550df65fc951eb8f4562b
Irene <grim6ace@gmail.com>
2025-07-01 00:49:45 +0300

    Ver 17

--- grid-layout.js
+++ grid-layout.js
@@ -1,8 +1,10 @@
-/* Copyright date */
+/* Copyright date 
+******************/
 const date = document.getElementById("copyright-date");
 date.innerHTML = new Date().getFullYear();
 
-/* Dark mode button */
+/* Dark mode button 
+********************/
 const switchBtn = document.querySelector('.switch-button');
 
 switchBtn.addEventListener('click', () => {
@@ -10,7 +12,8 @@ switchBtn.addEventListener('click', () => {
   document.documentElement.classList.toggle('dark-mode', darkModeOn); /* toggle('class', condition);documentElement returns <html> */
 });
 
-/* Dropdown menu (three bars button) */
+/* Dropdown menu (three bars button) 
+*************************************/
 const threeBarsBtn = document.querySelector('.three-bars-btn-div');
 const dropdownMenu = document.querySelector('.dropdown-menu'); 
 /* height:  233.353px */
@@ -24,4 +27,24 @@ window.addEventListener('click', (event) => {
   if (!dropdownMenu.contains(event.target) && !threeBarsBtn.contains(event.target)) {
     dropdownMenu.classList.remove('show');
   }
+});
+
+/* FAQ section
+***************/
+const questionCollapsibleBtns = document.querySelectorAll('.question-chevron-container');
+
+questionCollapsibleBtns.forEach((btn) => {
+  const collapsibleContainer = btn.parentElement;
+  const answerContent = collapsibleContainer.querySelector('.faq-answer'); 
+
+  btn.addEventListener("click", () => {
+    collapsibleContainer.classList.toggle('show-hide-content');
+
+    if (collapsibleContainer.classList.contains('show-hide-content')) {
+      let fullHeight = answerContent.scrollHeight + 'px';
+      answerContent.style.maxHeight = fullHeight;
+    } else {
+      answerContent.style.maxHeight = '0px';
+    }
+  });
 });
