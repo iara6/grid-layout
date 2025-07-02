2eb94e0d7372e8834fe1816858033ca1042152f8
Irene <grim6ace@gmail.com>
2025-07-03 01:44:34 +0300

    Ver 21: Gallery

--- grid-layout.js
+++ grid-layout.js
@@ -48,3 +48,44 @@ questionCollapsibleBtns.forEach((btn) => {
     }
   });
 });
+
+/* Grid view options (gallery)
+******************************/
+const columns = document.getElementsByClassName("image-column");
+const gridBtns = document.getElementsByClassName("grid-btn");
+
+let i;
+
+function oneColumn() {
+  for (i = 0; i < columns.length; i++) {
+    columns[i].style.flex = "100%";
+  }
+};
+
+function twoColumns() {
+  for (i = 0; i < columns.length; i++) {
+    columns[i].style.flex = "50%";
+  }
+};
+
+function fourColumns() {
+  for (i = 0; i < columns.length; i++) {
+    columns[i].style.flex = "25%";
+  }
+};
+
+gridBtns[0].addEventListener("click", oneColumn);
+gridBtns[1].addEventListener("click", twoColumns);
+gridBtns[2].addEventListener("click", fourColumns);
+
+for (let i = 0; i < gridBtns.length; i++) {
+  gridBtns[i].addEventListener('click', function () {
+    const current = document.querySelector(".active");
+
+    if (current) {
+      current.classList.remove("active");
+    }
+
+    this.classList.add("active");
+  });
+}
